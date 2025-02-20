a0b703a0117cd1a54c2cd0d046a20d10d0ce0c2b
Samson Teclezion <steclezion@gmail.com>
2025-02-20 00:22:51 -0800

    Remove blade layout files from tracking

--- resources/views/layouts/js_libs.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- Javascript Files
-    ================================================== -->
-    <script src="{{secure_asset('js/plugins.js')}}"></script>
-    <script src="{{secure_asset('js/designesia.js')}}"></script>
-    <script src="{{secure_asset('js/swiper.js')}}"></script>
-    <script src="{{secure_asset('js/custom-swiper-3.js')}}"></script>
-    <script src="{{secure_asset('js/custom-marquee.js')}}"></script>
-fasdfasdfsadf
