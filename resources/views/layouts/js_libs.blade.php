c992f9f7e4d56bab55382889182cb865dd8a0fea
Samson Teclezion <steclezion@gmail.com>
2025-02-20 00:15:41 -0800

    Changes made 2025-02-19

--- resources/views/layouts/js_libs.blade.php
+++ resources/views/layouts/js_libs.blade.php
@@ -5,4 +5,4 @@
     <script src="{{secure_asset('js/swiper.js')}}"></script>
     <script src="{{secure_asset('js/custom-swiper-3.js')}}"></script>
     <script src="{{secure_asset('js/custom-marquee.js')}}"></script>
-
+fasdfasdfsadf
