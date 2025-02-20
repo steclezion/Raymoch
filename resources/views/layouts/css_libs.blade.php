9da11ec9f1c3ad7d074157cf3780b5ba26d65d15
Samson Teclezion <steclezion@gmail.com>
2025-02-20 00:24:39 -0800

    blade layout files from tracking

---
+++ resources/views/layouts/css_libs.blade.php
@@ -0,0 +1,22 @@
+
+
+ <!-- CSS Files
+    ================================================== -->
+    {{-- <link rel="stylesheet" href="{{secure_asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bootstrap'">
+    <link rel="stylesheet" href="{{secure_asset('css/plugins.css')}}" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" rel="stylesheet" href="{{secure_asset('css/swiper.css')}}" rel="stylesheet" type="text/css" >
+    <link rel="stylesheet" href="{{secure_asset('css/style.css')}}" rel="stylesheet" type="text/css" >
+    <link href="{{secure_asset('css/coloring.css')}}" rel="stylesheet" type="text/css'" >
+    <!-- color scheme -->
+    <link rel="stylesheet" id="colors" href="{{secure_asset('css/colors/scheme-01.css')}}" rel="stylesheet" type="text/css'" >
+ --}}
+
+
+    <link href="{{ secure_asset('css/bootstrap.min.css')  }}" rel="stylesheet" type="text/css" id="bootstrap">
+    <link href="{{ secure_asset('css/plugins.css')  }}" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset('css/style.css')  }}" rel="stylesheet" type="text/css" >
+    <link href="{{ secure_asset('css/coloring.css')  }}" rel="stylesheet" type="text/css" >
+    <!-- color scheme -->
+    <link id="colors" href="{{secure_asset('css/colors/scheme-01.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" rel="stylesheet" href="{{  secure_asset('css/swiper.css')   }}" rel="stylesheet" type="text/css" >
+
