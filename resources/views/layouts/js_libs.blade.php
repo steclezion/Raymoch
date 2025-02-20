1eb7ef460428ab421d133e84b476a9b1c22fa594
Samson Teclezion <steclezion@gmail.com>
2025-02-20 00:13:58 -0800

    Changes made 2025-02-19

--- resources/views/layouts/js_libs.blade.php
+++ resources/views/layouts/js_libs.blade.php
@@ -1,8 +1,8 @@
 <!-- Javascript Files
     ================================================== -->
-    <script src="{{asset('js/plugins.js')}}"></script>
-    <script src="{{asset('js/designesia.js')}}"></script>
-    <script src="{{asset('js/swiper.js')}}"></script>
-    <script src="{{asset('js/custom-swiper-3.js')}}"></script>
-    <script src="{{asset('js/custom-marquee.js')}}"></script>
+    <script src="{{secure_asset('js/plugins.js')}}"></script>
+    <script src="{{secure_asset('js/designesia.js')}}"></script>
+    <script src="{{secure_asset('js/swiper.js')}}"></script>
+    <script src="{{secure_asset('js/custom-swiper-3.js')}}"></script>
+    <script src="{{secure_asset('js/custom-marquee.js')}}"></script>
 
